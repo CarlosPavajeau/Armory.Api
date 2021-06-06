e0d6adfffeea49b1697f41180591159167ad0c82
CarlosPavajeau <cantte098@gmail.com>
2021-06-05 19:54:10 -0500

    feat: Add Assembly and Reflection helpers

---
+++ src/Shared/Helpers/AssemblyHelper.cs
@@ -0,0 +1,20 @@
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Armory.Shared.Helpers
+{
+    public static class AssemblyHelper
+    {
+        private static readonly ConcurrentDictionary<string, Assembly> Assemblies = new();
+
+        public static Assembly GetInstance(string key)
+        {
+            return Assemblies.GetOrAdd(key, Assembly.Load(key));
+        }
+    }
+
+    public static class Assemblies
+    {
+        public const string Users = "Armory.Users";
+    }
+}
