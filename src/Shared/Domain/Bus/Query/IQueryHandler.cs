c4c9f9fe7ea2536e6041a81bec18b4e3e03a6b0a
CarlosPavajeau <cantte098@gmail.com>
2021-07-04 11:47:35 -0500

    refactor: reverse some changes

--- src/Shared/Domain/Bus/Query/IQueryHandler.cs
+++ src/Shared/Domain/Bus/Query/IQueryHandler.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Armory.Shared.Domain.Bus.Query
 {
-    public interface IQueryHandler<in TQuery, TResponse>
+    public interface IQueryHandler<in TQuery, TResponse> where TQuery : Query
     {
         Task<TResponse> Handle(TQuery query);
     }
