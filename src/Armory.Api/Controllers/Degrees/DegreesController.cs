ebedd263236aae4be7fc4c8aeb989d429b1e9c4e
CarlosPavajeau <cantte098@gmail.com>
2021-07-12 10:44:18 -0500

    feat: add degrees and troopers api controllers

---
+++ src/Armory.Api/Controllers/Degrees/DegreesController.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Armory.Api.Controllers.Degrees.Requests;
+using Armory.Degrees.Application;
+using Armory.Degrees.Application.Create;
+using Armory.Degrees.Application.Find;
+using Armory.Degrees.Application.SearchAll;
+using Armory.Shared.Domain.Bus.Command;
+using Armory.Shared.Domain.Bus.Query;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Armory.Api.Controllers.Degrees
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class DegreesController : ControllerBase
+    {
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryBus _queryBus;
+        private readonly IMapper _mapper;
+
+        public DegreesController(ICommandBus commandBus, IQueryBus queryBus, IMapper mapper)
+        {
+            _commandBus = commandBus;
+            _queryBus = queryBus;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterDegree([FromBody] CreateDegreeRequest request)
+        {
+            try
+            {
+                var command = _mapper.Map<CreateDegreeCommand>(request);
+                await _commandBus.Dispatch(command);
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DegreeResponse>>> GetDegrees()
+        {
+            var degrees = await _queryBus.Ask<IEnumerable<DegreeResponse>>(new SearchAllDegreesQuery());
+            return Ok(degrees);
+        }
+
+        private NotFoundObjectResult DegreeNotFound(int id)
+        {
+            ModelState.AddModelError("DegreeNotFound",
+                $"No se encontró ningun grado con el código '{id}'.");
+            return NotFound(new ValidationProblemDetails(ModelState));
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<DegreeResponse>> GetDegree(int id)
+        {
+            var degree = await _queryBus.Ask<DegreeResponse>(new FindDegreeQuery(id));
+            if (degree != null)
+            {
+                return Ok(degree);
+            }
+
+            return DegreeNotFound(id);
+        }
+    }
+}
---
+++ src/Armory.Api/Controllers/Troopers/TroopersController.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Armory.Api.Controllers.Troopers.Requests;
+using Armory.Shared.Domain.Bus.Command;
+using Armory.Shared.Domain.Bus.Query;
+using Armory.Troopers.Application;
+using Armory.Troopers.Application.Create;
+using Armory.Troopers.Application.Find;
+using Armory.Troopers.Application.SearchAll;
+using Armory.Troopers.Application.Update;
+using Armory.Troopers.Domain;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Armory.Api.Controllers.Troopers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class TroopersController : ControllerBase
+    {
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryBus _queryBus;
+        private readonly IMapper _mapper;
+
+        public TroopersController(ICommandBus commandBus, IQueryBus queryBus, IMapper mapper)
+        {
+            _commandBus = commandBus;
+            _queryBus = queryBus;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterTroop([FromBody] CreateTroopRequest request)
+        {
+            try
+            {
+                var command = _mapper.Map<CreateTroopCommand>(request);
+                await _commandBus.Dispatch(command);
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TroopResponse>>> GetTroopers()
+        {
+            var troopers = await _queryBus.Ask<IEnumerable<TroopResponse>>(new SearchAllTroopersQuery());
+            return Ok(troopers);
+        }
+
+        private NotFoundObjectResult TroopNotFound(string id)
+        {
+            ModelState.AddModelError("TroopNotFound",
+                $"No se encontró ninguna tropa con el código '{id}'.");
+            return NotFound(new ValidationProblemDetails(ModelState));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TroopResponse>> GetTroop(string id)
+        {
+            var troop = await _queryBus.Ask<TroopResponse>(new FindTroopQuery(id));
+            if (troop != null)
+            {
+                return Ok(troop);
+            }
+
+            return TroopNotFound(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTroop(string id, [FromBody] UpdateTroopRequest request)
+        {
+            try
+            {
+                var command = _mapper.Map<UpdateTroopCommand>(request);
+                await _commandBus.Dispatch(command);
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest();
+            }
+            catch (TroopNotFound)
+            {
+                return TroopNotFound(id);
+            }
+
+            return Ok();
+        }
+    }
+}
