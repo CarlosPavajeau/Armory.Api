11775b43795bedcf200d8a4fea4904aebe136dcf
CarlosPavajeau <cantte098@gmail.com>
2021-07-14 16:10:13 -0500

    feat: add missing error messages in request classes

--- src/Armory.Api/Controllers/Armament/Explosives/Requests/UpdateExplosiveRequest.cs
+++ src/Armory.Api/Controllers/Armament/Explosives/Requests/UpdateExplosiveRequest.cs
@@ -4,31 +4,31 @@ namespace Armory.Api.Controllers.Armament.Explosives.Requests
 {
     public class UpdateExplosiveRequest
     {
-        [Required(ErrorMessage = "")]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "El código del explosivo es requerido.")]
+        [MaxLength(50, ErrorMessage = "El código del referido no debe tener más de 50 caracteres.")]
         public string Code { get; set; }
 
-        [Required(ErrorMessage = "")]
-        [MaxLength(128)]
+        [Required(ErrorMessage = "El tipo de explosivo es requerido.")]
+        [MaxLength(128, ErrorMessage = "El tipo de explosivo no debe tener más de 128 caracteres.")]
         public string Type { get; set; }
 
-        [Required(ErrorMessage = "")]
-        [MaxLength(256)]
+        [Required(ErrorMessage = "El calibre del explosivo es requerido-")]
+        [MaxLength(256, ErrorMessage = "El calibre del explosivo no debe tener más de 256 caracteres.")]
         public string Caliber { get; set; }
 
-        [Required(ErrorMessage = "")]
-        [MaxLength(256)]
+        [Required(ErrorMessage = "La marca del explosivo es requerido.")]
+        [MaxLength(256, ErrorMessage = "La marca del explosivo no debe tener más de 256 caracteres.")]
         public string Mark { get; set; }
 
-        [Required(ErrorMessage = "")]
-        [MaxLength(256)]
+        [Required(ErrorMessage = "El lote del explosivo es requerido.")]
+        [MaxLength(256, ErrorMessage = "El lote del explosivo no debe tener más de 256 caracteres.")]
         public string Lot { get; set; }
 
-        [Required(ErrorMessage = "")]
-        [MaxLength(256)]
+        [Required(ErrorMessage = "El número de serie del explosivo es requerido.")]
+        [MaxLength(256, ErrorMessage = "El número de serie del explosivo no debe tener más de 256 caracteres.")]
         public string Series { get; set; }
 
-        [Required(ErrorMessage = "")]
+        [Required(ErrorMessage = "La cantidad disponible del explosivo es requerida.")]
         public int QuantityAvailable { get; set; }
     }
 }
