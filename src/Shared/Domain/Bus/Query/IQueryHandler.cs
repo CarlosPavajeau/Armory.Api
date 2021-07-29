4275fdb0c187c293c528d01e4e5c3ab08b80c728
Carlos Pavajeau <cantte098@gmail.com>
2021-07-29 09:00:11 -0500

    refactor: rewrite the IQueryHandler interface

--- src/Shared/Domain/Bus/Query/IQueryHandler.cs
+++ src/Shared/Domain/Bus/Query/IQueryHandler.cs
@@ -1,9 +1,9 @@
-using System.Threading.Tasks;
+using MediatR;
 
 namespace Armory.Shared.Domain.Bus.Query
 {
-    public interface IQueryHandler<in TQuery, TResponse> where TQuery : Query
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+        where TQuery : Query<TResponse>
     {
-        Task<TResponse> Handle(TQuery query);
     }
 }
