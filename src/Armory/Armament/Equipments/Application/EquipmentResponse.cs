fae6f00d084b1fa7cc54877b7c0b0ba36ac7cbe7
Carlos Pavajeau <cantte098@gmail.com>
2021-09-29 11:08:51 -0500

    refactor: change property order

--- src/Armory/Armament/Equipments/Application/EquipmentResponse.cs
+++ src/Armory/Armament/Equipments/Application/EquipmentResponse.cs
@@ -2,9 +2,9 @@ namespace Armory.Armament.Equipments.Application
 {
     public class EquipmentResponse
     {
+        public string Serial { get; init; }
         public string Type { get; init; }
         public string Model { get; init; }
-        public string Serial { get; init; }
         public int QuantityAvailable { get; init; }
     }
 }
--- src/Armory/Armament/Weapons/Application/WeaponResponse.cs
+++ src/Armory/Armament/Weapons/Application/WeaponResponse.cs
@@ -2,11 +2,11 @@ namespace Armory.Armament.Weapons.Application
 {
     public class WeaponResponse
     {
+        public string Serial { get; init; }
         public string Type { get; init; }
         public string Mark { get; init; }
         public string Model { get; init; }
         public string Caliber { get; init; }
-        public string Serial { get; init; }
         public int NumberOfProviders { get; init; }
         public int ProviderCapacity { get; init; }
         public string OwnerId { get; init; }
