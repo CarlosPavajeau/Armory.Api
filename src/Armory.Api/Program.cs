b0cd5ebbcf7b2cc6b55a02197370219aa5995d5a
Carlos Pavajeau <cantte098@gmail.com>
2021-10-04 09:21:13 -0500

    feat: add and configure Serilog

--- src/Armory.Api/Program.cs
+++ src/Armory.Api/Program.cs
@@ -1,7 +1,9 @@
 using Armory.Api.Extensions;
 using Armory.Shared.Infrastructure.Persistence.EntityFramework;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Armory.Api
 {
@@ -9,12 +11,24 @@ namespace Armory.Api
     {
         public static void Main(string[] args)
         {
+            ConfigureLogger();
             CreateHostBuilder(args).Build().MigrateDatabase<ArmoryDbContext>().Run();
+        }
+
+        private static void ConfigureLogger()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true).Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
         }
     }
