216422a6778ad37c6581a5ccfb440e8eddd33140
Carlos Pavajeau <cantte098@gmail.com>
2021-10-04 10:06:10 -0500

    feat: add validation rules
    Add validation rules for 'AddAssignedWeaponMagazineFormatItemRequest' and 'CreateAssignedWeaponMagazineFormatRequest'

---
+++ src/Armory.Api/Controllers/Formats/AssignedWeaponMagazineFormats/Validators/AddAssignedWeaponMagazineFormatItemRequestValidator.cs
@@ -0,0 +1,54 @@
+﻿using Armory.Api.Controllers.Formats.AssignedWeaponMagazineFormats.Requests;
+using FluentValidation;
+
+namespace Armory.Api.Controllers.Formats.AssignedWeaponMagazineFormats.Validators
+{
+    public class
+        AddAssignedWeaponMagazineFormatItemRequestValidator : AbstractValidator<
+            AddAssignedWeaponMagazineFormatItemRequest>
+    {
+        public AddAssignedWeaponMagazineFormatItemRequestValidator()
+        {
+            RuleFor(m => m.FormatId)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Formato No.");
+
+            RuleFor(m => m.TroopId)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Oficial, Suboficial o Soldado");
+
+            RuleFor(m => m.WeaponSerial)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Arma");
+
+            RuleFor(m => m.SafetyCartridge)
+                .NotNull()
+                .WithName("Cartucho de Seguridad");
+
+            RuleFor(m => m.VerifiedInPhysical)
+                .NotNull()
+                .WithName("Verificado en físico");
+
+            RuleFor(m => m.Novelty)
+                .NotNull()
+                .WithName("Novedad");
+
+            RuleFor(m => m.AmmunitionQuantity)
+                .NotNull()
+                .WithName("Cantidad munición");
+
+            RuleFor(m => m.AmmunitionLot)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Lote munición");
+
+            RuleFor(m => m.Observations)
+                .NotEmpty()
+                .NotEmpty()
+                .WithName("Observaciones");
+        }
+    }
+}
---
+++ src/Armory.Api/Controllers/Formats/AssignedWeaponMagazineFormats/Validators/CreateAssignedWeaponMagazineFormatRequestValidator.cs
@@ -0,0 +1,50 @@
+﻿using Armory.Api.Controllers.Formats.AssignedWeaponMagazineFormats.Requests;
+using FluentValidation;
+
+namespace Armory.Api.Controllers.Formats.AssignedWeaponMagazineFormats.Validators
+{
+    public class
+        CreateAssignedWeaponMagazineFormatRequestValidator : AbstractValidator<
+            CreateAssignedWeaponMagazineFormatRequest>
+    {
+        public CreateAssignedWeaponMagazineFormatRequestValidator()
+        {
+            RuleFor(m => m.Code)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Format No.");
+
+            RuleFor(m => m.Validity)
+                .NotNull()
+                .WithName("Vigencia");
+
+            RuleFor(m => m.SquadCode)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Código de Escuadrón");
+
+            RuleFor(m => m.FlightCode)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Código de Escuadrilla");
+
+            RuleFor(m => m.FireteamCode)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Cóidigo de Escuadra");
+
+            RuleFor(m => m.Warehouse)
+                .NotNull()
+                .WithName("Almacén de Armamento");
+
+            RuleFor(m => m.Date)
+                .NotNull()
+                .WithName("Fecha");
+
+            RuleFor(m => m.Comments)
+                .NotEmpty()
+                .NotNull()
+                .WithName("Comentarios");
+        }
+    }
+}
